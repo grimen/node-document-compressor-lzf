e65ad791829c3fb5c1e792c5c5eeece4902b6677
Jonas Grimfelt <grimen@gmail.com>
2013-01-13 21:18:54 +0100

    Init.

--- test/index.js
+++ test/index.js
@@ -1,127 +1,140 @@
-var helper = require('./helper'),
-    assert = helper.assert,
-    debug = helper.debug;
 
-var Compressor = require('..'),
-    compressor = new Compressor();
+var Compressor = require('../../node-document-compressor');
 
-// -----------------------
-//  Test
-// --------------------
+module.exports = Compressor.Spec('LZF', {
+  module: require('..'),
+  engine: require('lzf'),
+  options: {encoding: 'utf8'},
+  pack: function(v) { return require('lzf').compress(new Buffer(v)); },
+  unpack: require('lzf').decompress,
+  binary: true
+});
 
-module.exports = {
 
-  'LZF': {
-    'new': {
-      '()': function() {
-        assert.instanceOf ( compressor, require('..') );
+// var helper = require('./helper'),
+//     assert = helper.assert,
+//     debug = helper.debug;
 
-        Compressor.reset();
+// var Compressor = require('..'),
+//     compressor = new Compressor();
 
-        var compressor2 = new Compressor();
+// // -----------------------
+// //  Test
+// // --------------------
 
-        assert.equal ( compressor2.url, null );
-        assert.typeOf ( compressor2.options, 'object' );
-        assert.deepEqual ( compressor2.options.custom, undefined );
-      },
+// module.exports = {
 
-      '(options)': function() {
-        Compressor.reset();
+//   'LZF': {
+//     'new': {
+//       '()': function() {
+//         assert.instanceOf ( compressor, require('..') );
 
-        var compressor2 = new Compressor({custom: {foo: 'bar'}});
+//         Compressor.reset();
 
-        assert.equal ( compressor2.url, null );
-        assert.typeOf ( compressor2.options, 'object' );
-        assert.deepEqual ( compressor2.options.custom, {foo: 'bar'} );
-      }
-    },
+//         var compressor2 = new Compressor();
 
-    '.klass': function() {
-      assert.property ( compressor, 'klass' );
-      assert.equal ( compressor.klass, Compressor );
-    },
+//         assert.equal ( compressor2.url, null );
+//         assert.typeOf ( compressor2.options, 'object' );
+//         assert.deepEqual ( compressor2.options.custom, undefined );
+//       },
 
-    '.defaults': function() {
-      assert.property ( Compressor, 'defaults' );
+//       '(options)': function() {
+//         Compressor.reset();
 
-      assert.equal ( Compressor.defaults.url, null );
-      assert.typeOf ( Compressor.defaults.options, 'object' );
-    },
+//         var compressor2 = new Compressor({custom: {foo: 'bar'}});
 
-    '.options': function() {
-      assert.property ( Compressor, 'options' );
-      assert.typeOf ( Compressor.options, 'object' );
-      assert.deepEqual ( Compressor.options, {encoding: 'utf8'} );
-    },
+//         assert.equal ( compressor2.url, null );
+//         assert.typeOf ( compressor2.options, 'object' );
+//         assert.deepEqual ( compressor2.options.custom, {foo: 'bar'} );
+//       }
+//     },
 
-    '.reset()': function() {
-      assert.property ( Compressor, 'reset' );
-      assert.typeOf ( Compressor.reset, 'function' );
+//     '.klass': function() {
+//       assert.property ( compressor, 'klass' );
+//       assert.equal ( compressor.klass, Compressor );
+//     },
 
-      Compressor.options = {foo: "bar"};
-      assert.deepEqual ( Compressor.options, {foo: "bar"} );
+//     '.defaults': function() {
+//       assert.property ( Compressor, 'defaults' );
 
-      Compressor.reset();
+//       assert.equal ( Compressor.defaults.url, null );
+//       assert.typeOf ( Compressor.defaults.options, 'object' );
+//     },
 
-      assert.equal ( Compressor.url, null );
-    }
-  },
+//     '.options': function() {
+//       assert.property ( Compressor, 'options' );
+//       assert.typeOf ( Compressor.options, 'object' );
+//       assert.deepEqual ( Compressor.options, {encoding: 'utf8'} );
+//     },
 
-  'LZF.prototype': {
-    '#options': function() {
-      assert.property ( compressor, 'options' );
-      assert.typeOf ( compressor.options, 'object' );
-    },
+//     '.reset()': function() {
+//       assert.property ( Compressor, 'reset' );
+//       assert.typeOf ( Compressor.reset, 'function' );
 
-    '#engine': function() {
-      assert.property ( compressor, 'engine' );
-      assert.equal ( compressor.engine, require('lzf') );
-    },
+//       Compressor.options = {foo: "bar"};
+//       assert.deepEqual ( Compressor.options, {foo: "bar"} );
 
-    '#binary': function() {
-      assert.property ( compressor, 'binary' );
-      assert.equal ( compressor.binary, true );
-    },
+//       Compressor.reset();
 
-    '#compress': {
-      '': function() {
-        assert.property ( compressor, 'compress' );
-        assert.typeOf ( compressor.compress, 'function' );
-      },
+//       assert.equal ( Compressor.url, null );
+//     }
+//   },
 
-      '(uncompressed string)': function() {
-        var input = JSON.stringify({_id: 1, a: "foo", b: "bar"});
+//   'LZF.prototype': {
+//     '#options': function() {
+//       assert.property ( compressor, 'options' );
+//       assert.typeOf ( compressor.options, 'object' );
+//     },
 
-        assert.deepEqual ( compressor.compress(input), require('lzf').compress(new Buffer(input)) );
-      },
+//     '#engine': function() {
+//       assert.property ( compressor, 'engine' );
+//       assert.equal ( compressor.engine, require('lzf') );
+//     },
 
-      '(uncompressed object)': function() {
-        var input = {_id: 1, a: "foo", b: "bar"};
+//     '#binary': function() {
+//       assert.property ( compressor, 'binary' );
+//       assert.equal ( compressor.binary, true );
+//     },
 
-        assert.deepEqual ( compressor.compress(input), require('lzf').compress(new Buffer(JSON.stringify(input))) );
-      }
-    },
+//     '#compress': {
+//       '': function() {
+//         assert.property ( compressor, 'compress' );
+//         assert.typeOf ( compressor.compress, 'function' );
+//       },
 
-    '#decompress': {
-      '': function() {
-        assert.property ( compressor, 'decompress' );
-        assert.typeOf ( compressor.decompress, 'function' );
-      },
+//       '(uncompressed string)': function() {
+//         var input = JSON.stringify({_id: 1, a: "foo", b: "bar"});
 
-      '(compressed string)': function() {
-        var input = JSON.stringify({_id: 1, a: "foo", b: "bar"});
-        var compressed = require('lzf').compress(new Buffer(input));
+//         assert.deepEqual ( compressor.compress(input), require('lzf').compress(new Buffer(input)) );
+//       },
 
-        assert.deepEqual ( compressor.decompress(compressed), require('lzf').decompress(compressed) );
-      },
+//       '(uncompressed object)': function() {
+//         var input = {_id: 1, a: "foo", b: "bar"};
 
-      '(compressed object)': function() {
-        var input = {_id: 1, a: "foo", b: "bar"};
-        var compressed = require('lzf').compress(new Buffer(JSON.stringify(input)));
+//         assert.deepEqual ( compressor.compress(input), require('lzf').compress(new Buffer(JSON.stringify(input))) );
+//       }
+//     },
 
-        assert.deepEqual ( compressor.decompress(compressed), require('lzf').decompress(compressed) );
-      }
-    }
-  }
+//     '#decompress': {
+//       '': function() {
+//         assert.property ( compressor, 'decompress' );
+//         assert.typeOf ( compressor.decompress, 'function' );
+//       },
 
-};
+//       '(compressed string)': function() {
+//         var input = JSON.stringify({_id: 1, a: "foo", b: "bar"});
+//         var compressed = require('lzf').compress(new Buffer(input));
+
+//         assert.deepEqual ( compressor.decompress(compressed), require('lzf').decompress(compressed) );
+//       },
+
+//       '(compressed object)': function() {
+//         var input = {_id: 1, a: "foo", b: "bar"};
+//         var compressed = require('lzf').compress(new Buffer(JSON.stringify(input)));
+
+//         assert.deepEqual ( compressor.decompress(compressed), require('lzf').decompress(compressed) );
+//       }
+//     }
+//   }
+
+// };
