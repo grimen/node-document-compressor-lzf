1224d7ded66bbf28e92dd68ab5d4b82bfff653fe
Jonas Grimfelt <grimen@gmail.com>
2013-01-14 21:34:23 +0100

    Init.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 
-var Compressor = require('../../node-document-compressor');
+var Compressor = require('node-document-compressor');
 
 module.exports = Compressor.Spec('LZF', {
   module: require('..'),
